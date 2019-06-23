99eb3f10e6a199c05fd22a62b20e9e8bc156239f
hw20181102929 <51589984+hw20181102929@users.noreply.github.com>
2019-06-23 16:17:05 +0800

    Initial Commit

---
+++ shuchu/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  shuchu
+//
+//  Created by s20181102929 on 2019/6/23.
+//  Copyright © 2019 s20181102929. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
