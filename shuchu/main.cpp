28109415749d1a93e7c6e9c9619fee617b31e150
hw20181102929 <51589984+hw20181102929@users.noreply.github.com>
2019-06-23 16:17:40 +0800

    Update main.cpp
    c++

--- shuchu/main.cpp
+++ shuchu/main.cpp
@@ -1,15 +1,63 @@
-//
-//  main.cpp
-//  shuchu
-//
-//  Created by s20181102929 on 2019/6/23.
-//  Copyright © 2019 s20181102929. All rights reserved.
-//
-
 #include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+#include <cmath>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int a=n,b=0;
+    while(a>0){
+        a=a/10;
+        b++;
+    }
+    int s,k,l=1,i;
+    s=pow(10,b-1);
+    if(b==3){
+        while(n>0){
+            k=n/s;
+            n=n%s;
+            s=s/10;
+            if(l==1){
+                for(i=0;i<k;i++){
+                    cout<<"B";
+                }
+            }
+            if(l==2){
+                for(i=0;i<k;i++){
+                    cout<<"S";
+                }
+            }
+            if(l==3){
+                for(i=1;i<=k;i++){
+                    cout<<i;
+                }
+            }
+            l++;
+        }
+    }
+    l=1;
+    if(b==2){
+        while(n>0){
+            k=n/s;
+            n=n%s;
+            s=s/10;
+            if(l==1){
+                for(i=0;i<k;i++){
+                    cout<<"S";
+                }
+            }
+            if(l==2){
+                for(i=1;i<=k;i++){
+                    cout<<i;
+                }
+            }
+            l++;
+        }
+    }
+    if(b==1){
+        for(i=1;i<=n;i++){
+            cout<<i;
+        }
+    }
     return 0;
-}
+} 
